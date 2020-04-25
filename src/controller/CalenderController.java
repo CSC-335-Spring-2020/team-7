c7367859cb85b7cfc370e4838e5c3dc6efd2191d
Amin Sennour <aminsennour@gmail.com>
2020-04-25 15:33:04 -0700

    Basic method definitons in controller

--- src/controller/CalenderController.java
+++ src/controller/CalenderController.java
@@ -1,6 +1,9 @@
 package controller;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.UUID;
 
 
 /**
@@ -13,4 +16,18 @@ import java.util.Date;
 
 public class CalenderController {
 
+    public List<CalendarEvent> getEvents(Date date){
+        return model.getEvents(date);
+    }
+
+    public void addEvent(String title, Date date, int startTime, int endTime, String location, String notes){
+        int uuid = new UUID(title).getLeastSignificantBits();
+        CalendarEvent event = new CalenderEvent(title, date, startTime, endTime, location, notes);
+        model.addEvent(event);
+
+    }
+
+    public void removeEvent(CalendarEvent toRemove){
+        model.removeEvent(toRemove);
+    }
 }
