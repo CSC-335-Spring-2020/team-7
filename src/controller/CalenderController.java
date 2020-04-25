ca082f6b554c29ae22abf17bac55f3f90fca114a
Amin Sennour <aminsennour@gmail.com>
2020-04-25 11:23:31 -0700

    Init CONTROL_1

--- src/controller/CalenderController.java
+++ src/controller/CalenderController.java
@@ -1,4 +1,16 @@
 package controller;
 
+import java.util.Date;
+
+
+/**
+ * This is the controller for the Calender App.
+ *
+ * Purpose : To take data from the model, organize it, and send it to the view,
+ * as well as taking data from the view, and adding it to the model.
+ *
+ */
+
 public class CalenderController {
+
 }
