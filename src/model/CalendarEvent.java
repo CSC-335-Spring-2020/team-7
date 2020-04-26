bfb87a220ab6ad6ac0632a768b1659157127f2af
Joseph-Acevedo <joseph.acevedo@outlook.com>
2020-04-25 22:57:42 -0700

    Added all event fields, as well sa getters and setters

--- src/model/CalendarEvent.java
+++ src/model/CalendarEvent.java
@@ -1,4 +1,76 @@
 package model;
 
+import java.util.Date;
+
 public class CalendarEvent {
+
+    private String eventTitle;
+    private Date eventDate;
+    private Date startTime, endTime;
+    private String eventLocation;
+    private String eventNotes;
+    private int eventId;
+
+    public CalendarEvent(String title, Date date, Date start, Date end, int id) {
+        this.eventTitle = title;
+        this.eventDate = date;
+        this.startTime = start;
+        this.endTime = end;
+        this.eventId = id;
+        this.eventLocation = "";
+        this.eventNotes = "";
+    }
+
+    public String getTitle() {
+        return this.eventTitle;
+    }
+
+    public void setTitle(String title) {
+        this.eventTitle = title;
+    }
+
+    public Date getDate() {
+        return this.eventDate;
+    }
+
+    public void setDate(Date date) {
+        this.eventDate = date;
+    }
+
+    public Date getStartTime() {
+        return this.startTime;
+    }
+
+    public void setStartTime(Date start) {
+        this.startTime = start;
+    }
+
+    public Date getEndTime() {
+        return this.endTime;
+    }
+
+    public void setEndTime(Date end) {
+        this.endTime = end;
+    }
+
+    public String getLocation() {
+        return this.eventLocation;
+    }
+
+    public void setLocation(String location) {
+        this.eventLocation = location;
+    }
+
+    public String getNotes() {
+        return this.eventNotes;
+    }
+
+    public void setNotes(String notes) {
+        this.eventNotes = notes;
+    }
+
+    public int getEventId() {
+        return this.eventId;
+    }
+
 }
