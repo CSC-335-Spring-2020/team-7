6969c4489969cc2ddfccbacd20417da35d31c273
nflindenberg <60163208+nflindenberg@users.noreply.github.com>
2020-05-06 21:41:42 -0700

    Tests Added, Removed Some Unused Functions

--- src/test/CalendarTest.java
+++ src/test/CalendarTest.java
@@ -1,10 +1,13 @@
 package test;
 
+import controller.CalendarController;
+import javafx.scene.paint.Color;
 import model.CalendarEvent;
+import model.CalendarModel;
 import model.CalendarRecurringEvent;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import java.util.Date;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -54,5 +57,56 @@ public class CalendarTest {
         assertEquals(event.getOccurances(endDate,startDate).size(),0);
     }
 
+    @Test
+    public void testModel(){
+        TreeMap eventMap = new TreeMap<Date, List<CalendarEvent>>();
+        List recurringEvents = new ArrayList<CalendarRecurringEvent>();
+        Date start = new Date();
+        Date end = new Date();
+        CalendarEvent event = new CalendarEvent("TestEvent", start, start, end, "1");
+        CalendarRecurringEvent rEvent = new CalendarRecurringEvent("TestEvent", start, start, end, "2", 10);
+        CalendarModel m = new CalendarModel("Test", eventMap, recurringEvents);
+        m.getName();
+        m.getColor();
+        m.setName("Change");
+        m.setColor(Color.BLUE);
+        m.addEvent(start, event);
+        m.addEvent(start, event);
+        m.addRecurringEvent(rEvent);
+        m.getEvents(start);
+        m.removeEvent(start, rEvent);
+        m.getEvents(new Date(2000, 0, 1));
+        m.getEventList();
+        m.getRecurringEventList();
+        m.addRecurringEvent(rEvent);
+        m.removeRecurringEvent(rEvent);
+        m.removeEvent("1");
+        m.removeEvent(start, event);
+        m.getEvents(start);
+        m = new CalendarModel("Test", Color.BLACK);
+    }
 
+    @Test
+    public void testControllerButNotFileImports(){
+        TreeMap eventMap = new TreeMap<Date, List<CalendarEvent>>();
+        List recurringEvents = new ArrayList<CalendarRecurringEvent>();
+        CalendarModel m = new CalendarModel("Test", eventMap, recurringEvents);
+        CalendarController c = new CalendarController(m);
+        CalendarController d = new CalendarController(m);
+        Date start = new Date(2000, 3, 1);
+        Date middle = new Date(2000, 4, 1);
+        Date end = new Date(2000, 4, 10);
+        CalendarRecurringEvent rEvent = new CalendarRecurringEvent("TestEvent", start, start, end, "2", 10);
+        c.getName();
+        c.setName("Changed");
+        c.getColor();
+        c.setColor(Color.BLUE);
+        c.getEventsOnDay(start);
+        c.addEvent("Start", start, start, end, "Somewhere", "No");
+        c.getEventsOnDay(start);
+        c.addRecurringEvent("None", start, start, end, "","", 4);
+        c.toString();
+        boolean s = c.equals(d);
+        assertEquals(s, true);
+    }
 }
